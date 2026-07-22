f480f0644c23632341fac35638d27264448bfd7d
Savio Pereira Soares <saviopereirasoares6@gmail.com>
2026-07-21 23:30:53 -0300

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# About
+
+Coffee Rating & Log Tracker: Build a command-line script to log coffee bean names, brewing methods, and your custom ratings. Use basic CREATE TABLE, INSERT, and SELECT queries sorted by date or rating.
+
+# Used
+- Python
+- SQLite3
---
+++ model/bean.py
@@ -0,0 +1,63 @@
+from model.database import Database
+from sqlite3 import Error
+class Bean:
+    @staticmethod
+    def create_table():
+        try:
+            conn = Database.connect_db()
+            cursor = conn.cursor()
+            cursor.execute('CREATE TABLE IF NOT EXISTS beans(id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, brewing TEXT, rating INTEGER CHECK(rating BETWEEN 1 AND 10))')
+            conn.commit()
+            conn.close()
+            return True
+        except Error as e:
+            print(f'ERROR: {e}')
+            conn.rollback()
+            conn.close()
+            return False
+    @staticmethod
+    def insert_bean(name, brewing, rating):
+        try:
+            conn = Database.connect_db()
+            cursor = conn.cursor()
+            cursor.execute('INSERT INTO beans(name, brewing, rating) VALUES(?, ?, ?)', (name, brewing, rating))
+            conn.commit()
+            conn.close()
+            return True
+        except Error as e:
+            print(f'ERROR: {e}')
+            conn.rollback()
+            conn.close()
+            return False
+    @staticmethod
+    def select_bean(id):
+        try:
+            conn = Database.connect_db()
+            cursor = conn.cursor()
+            cursor.execute('SELECT * FROM beans WHERE id=?', (id,))
+            if cursor.fetchone():
+                return cursor.fetchone()
+            else:
+                return False
+        except Error as e:
+            print(f'ERROR: {e}')
+            conn.rollback()
+            conn.close()
+            return False
+    @staticmethod
+    def list_beans():
+        try:
+            conn = Database.connect_db()
+            cursor = conn.cursor()
+            cursor.execute('SELECT * FROM beans')
+            if cursor.fetchall():
+                db_list = cursor.fetchall
+                actual_list = [dict(line) for line in db_list]
+                return actual_list
+            else:
+                return False
+        except Error as e:
+            print(f'ERROR: {e}')
+            conn.rollback()
+            conn.close()
+            return False
