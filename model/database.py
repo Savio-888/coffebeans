05757fb714a5ad4ae0986ae0328d0a0133ee3d51
Savio Pereira Soares <saviopereirasoares6@gmail.com>
2026-09-13 17:48:16 -0300

    starting to make adaptations to the sqlalchemy ORM

--- model/database.py
+++ model/database.py
@@ -1,7 +1,3 @@
-import sqlite3
-class Database:
-    @staticmethod
-    def connect_db():
-        conn = sqlite3.connect('database.db')
-        conn.row_factory = sqlite3.Row
-        return conn
+from sqlalchemy import create_engine
+
+engine = create_engine('sqlite:///database.db')
